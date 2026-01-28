7c8280a53b4907d9c4cbc89889dcb3102959dd70
AlexPaul9187 <alexander.paul@aah.org>
2026-01-28 08:34:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.dataframe (data=my_dataframe, use_container_width=True)
 st.stop()
 
 #convert the snowpark dataframe to a Pandas dataframe so we can use the LOC function
-pd_df-my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
